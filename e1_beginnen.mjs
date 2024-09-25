8463ccf26131400a053bf34011122c979588805c
ilies-dev9 <iliesmazouz2005@gmail.com>
2024-09-25 11:04:48 +0200

    Il_e1_beginnen.mjs

--- e1_beginnen.mjs
+++ e1_beginnen.mjs
@@ -4,5 +4,3 @@ let age = '19';
 let favoriteGame = Tetris;
 
 console.log('Hallo '+ name +', ik hoor dat je '+ age +' jaar oud bent en dat je lievelingsgame '+ favoriteGame +' is.');
-
-process.exit();
