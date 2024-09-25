f276916604ddd03633e4fd2cbfe62ec814b0ca41
ilies-dev9 <iliesmazouz2005@gmail.com>
2024-09-25 11:04:23 +0200

    opteller.mjs Iliès

--- e3_opteller.mjs
+++ e3_opteller.mjs
@@ -4,10 +4,10 @@ import{stdin as input, stdout as output} from 'node:process';
 const userInput = readline.createInterface({input, output});
 // Einde van userInput voorbereiden
 
-
-
 // Voeg hier je eigen code in
+let getal1 = parseFloat(await userInput.question());
+let getal2 = parseFloat(await userInput.question());
 
-
+console.log(getal1 + getal2);
 
 process.exit();
