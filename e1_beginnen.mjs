8e9b6e3490d3103ac2253a297eda343e80bcf6bc
ilies-dev9 <iliesmazouz2005@gmail.com>
2024-09-25 10:35:54 +0200

    beginnen.mjs Iliès

--- e1_beginnen.mjs
+++ e1_beginnen.mjs
@@ -1,5 +1,12 @@
 // Voeg hier je eigen code in
+import * as readline from 'node:readline/promises';
+import {stdin as input, stdout as output} from 'node:process';
+const userInput = readline.createInterface({input, output});
 
-
+let name = await userInput.question('Wat is je naam? ');
+let age = parseFloat(await userInput.question('Wat is jouw leeftijd? ');
+let favoriteGame = await userInput.question('Wat is jouw favoriete game? ');
 
 console.log('Hallo '+ name +', ik hoor dat je '+ age +' jaar oud bent en dat je lievelingsgame '+ favoriteGame +' is.');
+
+process.exit();
