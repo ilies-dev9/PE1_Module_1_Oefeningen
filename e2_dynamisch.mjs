77e01109345043e5c557f2a7f15749a771e07642
ilies-dev9 <iliesmazouz2005@gmail.com>
2024-09-25 10:51:13 +0200

    dynamisch.mjs Iliès

--- e2_dynamisch.mjs
+++ e2_dynamisch.mjs
@@ -7,8 +7,9 @@ const userInput = readline.createInterface({input, output});
 
 
 // Voeg hier je eigen code in
-
-
+let name = await userInput.question('Wat is je naam? ');
+let age = parseFloat(await userInput.question('Wat is jouw leeftijd? ');
+let favoriteGame = await userInput.question('Wat is jouw favoriete game? ');
 
 console.log('Hallo '+ name +', ik hoor dat je '+ age +' jaar oud bent en dat je lievelingsgame '+ favoriteGame +' is.');
 
